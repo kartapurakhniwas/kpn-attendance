532f3d9037db1b14ea87eece1fc3097bf961690d
Akaaljot Singh <akaaljotsingh@gmail.com>
2024-05-29 11:35:27 +0530

    refactor

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ImportOrdersComponent } from './pages/orders/import-orders/import-orders.component';
-import { ImportOrdersDetailComponent } from './pages/orders/import-orders/import-orders-detail.component';
-import { ImportExportContainerComponent } from './pages/orders/import-export-container/import-export-container.component';
-import { ImportExportContainerDetailComponent } from './pages/orders/import-export-container/import-export-container-detail.component';
-import { DriverLogComponent } from './pages/driver-log/driver-log.component';
-import { CustomersComponent } from './pages/customers/customers.component';
-import { ShippingLineComponent } from './pages/shipping-line/shipping-line.component';
-import { AddShippingLineComponent } from './pages/shipping-line/add-shipping-line.component';
 import { UserComponent } from './ui/layouts/user/user.component';
-import { ExportOrdersDetailComponent } from './pages/orders/export-order/export-orders-detail.component';
-import { ExportOrdersComponent } from './pages/orders/export-order/export-orders.component';
 import { LocationsComponent } from './pages/locations/locations.component';
 import { AddLocationsComponent } from './pages/locations/add-locations.component';
-import { AddCustomersComponent } from './pages/customers/add-customers.component';
 
 export const routes: Routes = [
     {
@@ -34,52 +23,8 @@ export const routes: Routes = [
                 component: DashboardComponent
             },
             {
-                path: 'import-orders',
-                component: ImportOrdersComponent
-            },
-            {
-                path: 'import-orders/details/:guid',
-                component: ImportOrdersDetailComponent
-            },
-            {
-                path: 'export-orders',
-                component: ExportOrdersComponent
-            },
-            {
-                path: 'export-orders/details/:guid',
-                component: ExportOrdersDetailComponent
-            },
-            {
-                path: 'import-export-container',
-                component: ImportExportContainerComponent
-            },
-            {
-                path: 'import-export-container/filter/:id',
-                component: ImportExportContainerComponent
-            },
-            {
-                path: 'import-export-container/details',
-                component: ImportExportContainerDetailComponent
-            },
-            {
-                path: 'driver-log',
-                component: DriverLogComponent
-            },
-            {
-                path: 'customers',
-                component: CustomersComponent
-            },
-            {
-                path: 'customers/add',
-                component: AddCustomersComponent
-            },
-            {
-                path: 'shipping-line',
-                component: ShippingLineComponent
-            },
-            {
-                path: 'shipping-line/add',
-                component: AddShippingLineComponent
+                path: 'login',
+                component: LoginComponent
             },
             {
                 path: 'locations',
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [MatInputModule, MatFormFieldModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
